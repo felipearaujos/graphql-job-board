67773cf0f66b86391ab803461b8c403a459d9f31
Felipe Araujo <felipe.araujo.s@live.com>
2020-02-02 18:07:39 -0300

    list jobs in company page

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,16 @@
 const db = require('./db')
 
 const Query = {
-    job: (root, {id}) => db.jobs.get(id),
+    company: (root, {id}) => db.companies.get(id),
+    job: (root, {id}) => db.jobs.get(id),    
     jobs: () => db.jobs.list()
     
 };
+
+const Company = {
+    jobs: (company) => db.jobs.list()
+      .filter((job) => job.companyId === company.id)
+  };
 
 const Job = {
     company: (job) => db.companies.get(job.companyId)
@@ -12,4 +18,4 @@ const Job = {
 
 
 
-module.exports = { Query, Job };
+module.exports = { Query, Company, Job };
